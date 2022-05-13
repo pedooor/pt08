be2c32b552a8a7a67c64309f76ce8bf52933109a
Pedro Pinto <100382804+pedooor@users.noreply.github.com>
2022-05-13 09:03:10 +0100

    Add files via upload

---
+++ scripts/repetir.js
@@ -0,0 +1,5 @@
+alert("pronto?") /* mostra ao utilizador uma mensagem e um botão de confirmação de leitura da mensagem.*/
+for (count = 1; count <= 500; count++) /* um for-loop executa certo comando um certo numero de vezes. Neste caso, temos “count = 1;” que nos diz que a contagem começará sendo igual a 1. 
+“count <= 500;” que podemos traduzir em “continue enquanto a contagem for menor ou igual a 500”. Finalmente, “count++” diz que depois de executarmos o comando o for-loop contém, incremente (aumente em um) o valor de count.*/
+    document.write("<br>" + count + "Eu não vou atirar aviões de papel na aula.") /*escreve no documento HTML, de forma numerada, a frase definida, 500 vezes*/
+
